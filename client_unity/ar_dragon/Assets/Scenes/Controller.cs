84028554c271835fd29d1880c08ed457e5a12917
fcx <fcx@pingxingyun.com>
2022-04-20 14:32:13 +0800

    add cloudapp demo

---
+++ client_unity/ar_dragon/Assets/Scenes/Controller.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Animator animator;
+
+    private Vector3 oriScale = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(animator != null);
+
+        oriScale = animator.transform.localScale;
+
+        RandomTrigger();
+
+        lark.LarkManager larkManager = lark.LarkManager.Instance;
+        larkManager.DataChannel.onTaskStatus += OnTaskStatus;
+        larkManager.DataChannel.onConnected += OnConnected;
+        larkManager.DataChannel.onText += OnTextMessage;
+        larkManager.DataChannel.onBinary += OnBinaryMessaeg;
+        larkManager.DataChannel.onClose += OnClose;
+        // 智能语音相关回调 
+        larkManager.DataChannel.onAiVoiceURL += OnAiVoiceURL;
+        larkManager.DataChannel.onAiVoiceStream += OnAiVoiceStream;
+
+        // start connect
+        lark.DataChannelNativeApi.ApiRestult restult = lark.LarkManager.Instance.StartConnect();
+
+        Debug.Log("Connect Result " + restult); ;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W)) {
+            Walk();
+        }
+        if (Input.GetKeyUp(KeyCode.F)) {
+            Fly();
+        }
+        if (Input.GetKeyUp(KeyCode.R)) {
+            Run();
+        }
+        if (Input.GetKeyUp(KeyCode.I)) {
+            Idel();
+        }
+    }
+
+    void OnDestroy()
+    {
+    }
+    #region trigger
+    void RandomTrigger()
+    {
+        string[] triggers = { "walk", "fly", "run", "idel" };
+        Trigger(triggers[UnityEngine.Random.Range(0, triggers.Length)]);
+    }
+
+    void Walk()
+    {
+        Trigger("walk");
+    }
+
+    void Fly()
+    {
+        Trigger("fly");
+    }
+
+
+    void Run()
+    {
+        Trigger("run");
+    }
+
+    void Idel()
+    {
+        Trigger("idel");
+    }
+
+    void Trigger(string trigger) {
+        if (animator == null)
+        {
+            return;
+        }
+        animator.SetTrigger(trigger);
+    }
+    #endregion
+
+    #region callbacks
+    [System.Serializable]
+    public class JsonCmd
+    {
+        public enum CmdType
+        {
+            TOUCH_DOWN = 1000,
+            TOUCH_MOVE = 1001,
+            TOUCH_UP   = 1002,
+        }
+
+        public static JsonCmd ParseJsonCmd(string res)
+        {
+            return JsonUtility.FromJson<JsonCmd>(res);
+        }
+
+        public CmdType type;
+        public float x = 0;
+        public float y = 0;
+
+        public JsonCmd(CmdType type, float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    }
+
+    public void OnTextMessage(string msg)
+    {
+        Debug.Log("OnTextMessage " + msg);
+        try
+        {
+            JsonCmd cmd = JsonCmd.ParseJsonCmd(msg);
+            if (cmd.type == JsonCmd.CmdType.TOUCH_UP)
+            {
+                RandomTrigger();
+            }
+        } catch (Exception)
+        {
+            Debug.Log("unkown json message " + msg);
+        }
+    }
+
+    public void OnTaskStatus(bool status, string taskId)
+    {
+        Debug.Log("on task status change " + status + " " + taskId);
+    }
+    public void OnConnected()
+    {
+        Debug.Log("OnConnected ");
+    }
+    public void OnBinaryMessaeg(byte[] binary)
+    {
+        Debug.Log("OnBinaryMessaeg " + BitConverter.ToString(binary));
+    }
+    public void OnClose(lark.DataChannelNativeApi.ErrorCode code)
+    {
+        Debug.Log("OnClose " + code);
+    }
+    public void OnAiVoiceURL(lark.DataChannelNativeApi.AiVoiceURL aiVoiceURL)
+    {
+        Debug.Log("OnAiVoiceURL " + aiVoiceURL.nlg + " url " + aiVoiceURL.online_url);
+    }
+
+    public void OnAiVoiceStream(lark.DataChannelNativeApi.AiVoiceStream aiVoiceStream)
+    {
+        Debug.Log("OnAiVoiceURL " + aiVoiceStream.nlg);
+    }
+    #endregion
+}
---
+++ client_unity/ar_dragon/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    const int ROW_MAX = 5;
+    const int LINE_MAX = 5;
+    const int PLANE_MAX = ROW_MAX * LINE_MAX;
+
+    public Transform objectContainer;
+    public GameObject cubeBase;
+
+    List<GameObject> cubeList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 100; i++) {
+            CreateCube();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateCube()
+    {
+        if (objectContainer == null)
+        {
+            return;
+        }
+        if (cubeBase == null)
+        {
+            return;
+        }
+        int index = cubeList.Count + 1;
+        int d = index / PLANE_MAX;
+        int planeIndex = index % PLANE_MAX;
+        int r = planeIndex / LINE_MAX;
+        int l = planeIndex % LINE_MAX;
+        GameObject cube = Instantiate(cubeBase, objectContainer);
+        cube.transform.position = new Vector3(2 * l - 5, 2 * r - 5, d * 2);
+        cubeList.Add(cube);
+    }
+}
